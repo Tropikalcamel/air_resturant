015bdbb082432a72ac219f0a6b5de48db899cddd
marcus meier <marcusxmeier@gmail.com>
2024-02-27 17:33:58 +0100

    cancel bookings

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,9 @@
+# app/models/booking.rb
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :restaurant
+
+  enum status: [:pending, :confirmed, :canceled]
+
+  # Add any other necessary validations or methods
 end
