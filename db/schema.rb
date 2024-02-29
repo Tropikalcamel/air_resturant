a64e18dfd4feff5f065437d4164a61dab293dde4
tarik <tarik.elmoudden@gmail.com>
2024-02-29 18:55:37 +0100

    We fix seedfile and booking controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.1].define(version: 2024_02_29_133823) do
-
-
+ActiveRecord::Schema[7.1].define(version: 2024_02_29_162610) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -44,6 +41,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_29_133823) do
     t.float "ratings"
     t.integer "user_ratings_total"
     t.text "category"
+    t.string "dietaryRestrictions"
     t.index ["user_id"], name: "index_restaurants_on_user_id"
   end
 
